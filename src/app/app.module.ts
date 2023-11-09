c5b26c83b345d6ffd0a7f2adb9f2a2dac865b612
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2023-11-09 22:25:09 +0100

    feat: Registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { StakeholdersModule } from './feature-modules/stakeholders/stakeholders.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StakeholdersModule,
+  ],
   exports: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
